9b5e89402ba808cdab78056cc17f4dd8c3fbfac3
harrysudana <harrysudana@gmail.com>
2019-10-24 20:38:07 +0800

    apply component passport

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,16 @@
             @endif
 
             <div class="content">
+
+            <!-- let people make clients -->
+<passport-clients></passport-clients>
+
+<!-- list of clients people have authorized to access our account -->
+<passport-authorized-clients></passport-authorized-clients>
+
+<!-- make it simple to generate a token right in the UI to play with -->
+<passport-personal-access-tokens></passport-personal-access-tokens>
+
                 <div class="title m-b-md">
                     Laravel
                 </div>
